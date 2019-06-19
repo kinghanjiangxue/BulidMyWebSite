c2f853fe872cab3c67f390dc9b39820116b9ce61
babysadangel <yuankisstherain@163.com>
2019-06-19 18:48:44 +0800

    视图修改，Markdown使用

--- article/urls.py
+++ article/urls.py
@@ -13,5 +13,7 @@ app_name = 'article'
 urlpatterns = [
 
     # path函数将url映射到视图
-    path('article-list', views.article_list, name = 'article_list'),
+    path('article-list', views.article_list, name='article_list'),
+    # 文章详情
+    path('article-detail/<int:pk>/', views.article_detail, name='article_detail'),
 ]
--- article/views.py
+++ article/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from .models import ArticlePost
+import markdown
 
 
+# 文章列表
 def article_list(request):
 
     # 取出所有的博客文章
@@ -11,3 +13,21 @@ def article_list(request):
     # render函数，载入模板并返回context对象
     return render(request, 'article/list.html', context)
 
+
+# 文章详情
+def article_detail(request, pk):
+    # 取出所有文章
+    article = ArticlePost.objects.get(pk=pk)
+
+    # 将Markdown语法渲染成HTML的样式
+    article.body = markdown.markdown(article.body,
+                                     extensions=[
+                                         # 包含缩写，表格等常用扩展
+                                         'markdown.extensions.extra',
+                                         # 语法高亮扩展
+                                         'markdown.extensions.codehilite',
+                                     ])
+    # 需要传递给模板的对象
+    context = {'article': article}
+    # 载入模板，并返回context对象
+    return render(request, 'article/detail.html', context)
