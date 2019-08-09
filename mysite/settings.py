a01878a4047c7287bd1625eb92e9f92305e395df
wangyuanyuan <wangyuanyuan@7daichina.com>
2019-08-09 18:15:59 +0800

    日子记录

--- mysite/settings.py
+++ mysite/settings.py
@@ -169,19 +169,58 @@ CKEDITOR_CONFIGS = {
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
+    'formatters':  {
+        'verbose':{
+            'format': '{levelname} {asctime} {module} {process:d} {thread:d} {message}',
+            'style': '{',
+        },
+        'simple':{
+            'format': '{levelname} {message}',
+            'style': '{',
+        },
+    },
+
+    'filters': {
+        'require_debug_true': {
+            '()': 'django.utils.log.RequireDebugTrue',
+        }
+    },
+
     'handlers': {
+
+        'console': {
+            'level':'INFO',
+            'filters': ['require_debug_true'],
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple',
+        },
+
+        'main_admins':{
+            'level': 'ERROR',
+            'class': 'django.utils.log.AdminEmailHandler',
+            'fromatter': 'verbose',
+        },
+
+
         'file': {
             'level': 'ERROR',
             'class': 'logging.FileHandler',
             'filename': os.path.join(BASE_DIR, 'logs/debug.log'),
+            'formatter': 'verbose',
         },
     },
 
     'loggers': {
-        'django': {
-            'handlers': ['file'],
-            'level': 'ERROR',
+
+        'django':{
+            'handlers':['console'],
             'propagate': True,
+        },
+
+        'django.request': {
+            'handlers': ['file', 'mail_admins'],
+            'level': 'WARNING',
+            'propagate': False,
         },
     },
 }
